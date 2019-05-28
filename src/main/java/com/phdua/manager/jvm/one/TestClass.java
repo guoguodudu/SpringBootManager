fd4db4214d4eb058d926f3e5e6acdfdd1d5c3375
dupihua <2521680378@qq.com>
2019-05-28 13:35:14 +0800

    线程池-------https://github.com/forezp/SpringCloudLearning.git

---
+++ src/main/java/com/phdua/manager/jvm/one/TestClass.java
@@ -0,0 +1,15 @@
+package com.phdua.manager.jvm.one;
+
+public class TestClass {
+
+    private int m;
+
+    public  int inc(){
+        return m+1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+
+}
